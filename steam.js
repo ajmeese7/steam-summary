4527302f9c6750cc6262b9c80a8b826562a2dccb
Aaron Meese <ajmeese7@gmail.com>
2018-04-14 16:33:36 -0500

    Displays name
    Now the player's real name is displayed in small text next to their username (if they have it set)

--- steam.js
+++ steam.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   var input = document.getElementById("input");
   var username = "";
+  // TODO: Add an option to input other Steam forms (SteamID, Steam64, Steam32)
   input.onkeyup = function(e) {
     // Runs on enter press (when key is released)
     if (e.keyCode == 13) {
@@ -30,19 +31,30 @@ function validate(username) {
 }
 
 function getID(username) {
-  $.getJSON('/proxy.php', {username: username}, function (response) {
+  // Replace '/steam' with the directory on your server in which proxy.php is located
+  $.getJSON('/steam/proxy.php', {username: username}, function (response) {
     // TODO: ADD FALLACY PROTECTION AGAINST INVALID USERNAMES!
     getData(response.response.steamid);
   });
 }
 
 function getData(id) {
-  $.getJSON('/proxy.php', {steamid: id}, function (response) {
+  $.getJSON('/steam/proxy.php', {steamid: id}, function (response) {
+    // A detailed list of all accessible data can be found here:
+    // https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_.28v0002.29
     var info = response.response.players[0];
     var body = document.getElementsByTagName("body")[0];
+    
     body.innerHTML = "<img id='photo' src='" + info.avatarfull + "' />";
     body.style.backgroundColor = "#eee9df";
 
-    body.innerHTML += "<p id='name'>" + info.personaname + "</p>";
+    body.innerHTML += "<p id='name'>" + info.personaname + " </p>";
+    var nameP = document.getElementById("name");
+
+    // TODO: Test the value returned when a profile doesn't have a real name set!
+    var realName = info.realname;
+    if (realName != "") {
+      nameP.innerHTML += "<small>(" + info.realname + ")</small>";
+    }
   });
 }
